08ad91d8388f837fa1632e8107f523fb59577d28
Cesar Espino <6097531+cespino@users.noreply.github.com>
2018-10-19 11:47:53 -0500

    added properties to RosterMember

--- iMISClubs/iMISClubs/Models/RosterMember.cs
+++ iMISClubs/iMISClubs/Models/RosterMember.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 
 namespace iMISClubs.Models
 {
@@ -7,5 +8,16 @@ namespace iMISClubs.Models
         public string Id { get; set; }
         public string FullName { get; set; }
         public string Description { get; set; }
+        public string InstituteTypeName { get; set; }
+        public string InstituteType { get; set; }
+        public CheckInStatus Status { get; set; }
+        public Collection<MemberAlert> MemberAlerts { get; set; }
+    }
+
+    public enum CheckInStatus
+    {
+        New,
+        CheckedIn,
+        CheckedOut
     }
 }
